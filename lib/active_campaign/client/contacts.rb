400695baa201b40102dc825f65dd349c1f64cd6e
Mikael Henriksson <mikael@zoolutions.se>
2015-05-13 20:58:36 +0200

    Syntax error

--- lib/active_campaign/client/contacts.rb
+++ lib/active_campaign/client/contacts.rb
@@ -4,16 +4,15 @@ module ActiveCampaign
   class Client
     module Contacts
       CONTACT_METHODS = %w(
-      CONTACT_METHODS = %w(
-        add automation_list delete_list delete edit list paginator 
+        add automation_list delete_list delete edit list paginator
         sync view view_email view_hash tag_add tag_remove
       )
       CONTACT_POST_METHODS = %w(add edit sync tag_add tag_remove)
 
       extend ActiveSupport::Concern
 
-      # TODO: Create proper methods with parameter validation and possible naming fixes since
-      #       this is one the worst APIs I have ever worked with.
+      # TODO: Create proper methods with parameter validation and possible naming
+      #       fixes since this is one the worst APIs I have ever worked with.
       included do
         %w(contact subscriber).each do |name|
           CONTACT_METHODS.each do |method|
