013a3e10497f71841fdef3e571eed6074a748916
Thiago Dias <tdtadeu@gmail.com>
2018-08-16 18:34:31 -0300

    Fix method_missing for ActiveCampaign module

--- lib/active_campaign.rb
+++ lib/active_campaign.rb
@@ -34,8 +34,10 @@ module ActiveCampaign
 
   private
 
-  def method_missing(method_name, *args, &block)
-    return super unless client.respond_to?(method_name)
-    client.send(method_name, *args, &block)
+  class << self
+    def method_missing(method_name, *args, &block)
+      return super unless client.respond_to?(method_name)
+      client.send(method_name, *args, &block)
+    end
   end
 end
