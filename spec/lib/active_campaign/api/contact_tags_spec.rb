535bb4f7d4361df96ec35efda0888764d3d481ec
timchaston <timchaston@gmail.com>
2020-06-11 15:56:03 +1000

    Add show methods for V3 endpoints for contact tags (#69)

--- spec/lib/active_campaign/api/contact_tags_spec.rb
+++ spec/lib/active_campaign/api/contact_tags_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe ActiveCampaign::API::ContactTags, :vcr do
       expect(response).to eq({})
     end
   end
+
+  describe '#show_contact_tags_for_contact', :with_existing_contact_tag do
+    subject(:response) { client.show_contact_contact_tags(contact_id) }
+
+    it 'returns an array of contact tag hashes' do
+      expect(response).to include_json(contact_tags: [expected_contact_tag_response])
+    end
+  end
+
+  describe '#show_contact_tag', :with_existing_contact_tag do
+    subject(:response) { client.show_contact_tag(contact_tag_id) }
+
+    it 'returns a contact tag hash' do
+      expect(response).to include_json(contact_tag: expected_contact_tag_response)
+    end
+  end
+
+  describe '#show_contact_tags', :with_existing_contact_tag do
+    subject(:response) { client.show_contact_tags }
+
+    it 'returns an array of contact tag hashes' do
+      expect(response).to include_json(contact_tags: [expected_contact_tag_response])
+    end
+  end
 end
