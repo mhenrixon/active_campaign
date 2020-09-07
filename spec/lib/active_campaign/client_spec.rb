2f3a47d69cd73cbd2681ce5c8771ca9adc87a5cb
Ryan Winograd <ryan@thewinograds.com>
2020-09-07 13:31:21 -0500

    Satisfy linter: prefer single quotes

--- spec/lib/active_campaign/client_spec.rb
+++ spec/lib/active_campaign/client_spec.rb
@@ -3,8 +3,8 @@
 RSpec.describe ActiveCampaign::Client, :vcr do
   describe '.new' do
     it 'can be used to intialize a client object that can make requests' do
-      WebMock.stub_request(:get, "http://fake.com/").with(body: "{}")
-      described_class.new.get("http://fake.com/")
+      WebMock.stub_request(:get, 'http://fake.com/').with(body: '{}')
+      described_class.new.get('http://fake.com/')
     end
   end
 end
