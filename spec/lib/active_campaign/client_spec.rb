0b7eeabfbc1a26ccd7ee38633fd5bb61c6c0516c
Ryan Winograd <ryan@thewinograds.com>
2020-09-10 09:56:14 -0500

    Fix spec

--- spec/lib/active_campaign/client_spec.rb
+++ spec/lib/active_campaign/client_spec.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
-RSpec.describe ActiveCampaign::Client, :vcr do
+RSpec.describe ActiveCampaign::Client do
   describe '.new' do
     it 'can be used to intialize a client object that can make requests' do
-      WebMock.stub_request(:get, 'http://fake.com/').with(body: '{}')
+      stub = WebMock.stub_request(:get, 'http://fake.com/').to_return(body: '{}')
+
       described_class.new.get('http://fake.com/')
+
+      expect(stub).to have_been_requested
     end
   end
 end
