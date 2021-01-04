3787e769741d3f3b789e4f8de9cf7b29acbef376
janmistarz <jan.mistarz@infiniteloop.eu>
2021-01-04 16:06:17 +0100

    added bulk import

--- spec/lib/active_campaign/api/contacts_spec.rb
+++ spec/lib/active_campaign/api/contacts_spec.rb
@@ -25,9 +25,9 @@ RSpec.describe ActiveCampaign::API::Contacts, :vcr do
     subject(:response) { client.create_contact(contact_params) }
 
     let(:contact_first_name) { 'Hokus' }
-    let(:contact_last_name)  { 'Pokus' }
-    let(:contact_email)      { 'mikael@mhenrixon.com' }
-    let(:contact_phone)      { '+491735728523' }
+    let(:contact_last_name) { 'Pokus' }
+    let(:contact_email)     { 'mikael@mhenrixon.com' }
+    let(:contact_phone)     { '+491735728523' }
     let(:contact_params) do
       {
         email: contact_email,
@@ -57,9 +57,9 @@ RSpec.describe ActiveCampaign::API::Contacts, :vcr do
     subject(:response) { client.update_contact(contact_id, update_params) }
 
     let(:new_contact_first_name) { 'Henrik' }
-    let(:new_contact_last_name)  { 'Mikaelsson' }
-    let(:new_contact_email)      { 'henrik@mikaelsson.com' }
-    let(:new_contact_phone)      { '+46730393204' }
+    let(:new_contact_last_name) { 'Mikaelsson' }
+    let(:new_contact_email)     { 'henrik@mikaelsson.com' }
+    let(:new_contact_phone)     { '+46730393204' }
     let(:update_params) do
       {
         first_name: new_contact_first_name,
@@ -88,8 +88,8 @@ RSpec.describe ActiveCampaign::API::Contacts, :vcr do
     subject(:response) { client.sync_contact(sync_params) }
 
     let(:new_contact_first_name) { 'Henrik' }
-    let(:new_contact_last_name)  { 'Mikaelsson' }
-    let(:new_contact_phone)      { '+46730393204' }
+    let(:new_contact_last_name) { 'Mikaelsson' }
+    let(:new_contact_phone)     { '+46730393204' }
     let(:sync_params) do
       {
         first_name: new_contact_first_name,
@@ -131,4 +131,29 @@ RSpec.describe ActiveCampaign::API::Contacts, :vcr do
       )
     end
   end
+
+  #   describe '#bulk_import_contacts' do
+  #     subject(:response) { client.bulk_import_contacts(contacts) }
+  #
+  #     let(:contacts) do
+  #       [
+  #         { email: 'test_user_1@test.com',
+  #           first_name: 'test_user_1',
+  #           last_name: 'test_user_1',
+  #           phone: 123_123_123,
+  #           tags: 'test_tag1'
+  #         },
+  #         { email: 'test_user_2@test.com',
+  #           first_name: 'test_user_2',
+  #           last_name: 'test_user_2',
+  #           phone: 123_123_123,
+  #           tags: 'test_tag2' }
+  #       ]
+  #     end
+  #
+  #     it 'returns success message' do
+  #       expect(response).to include_json('{"Success":1,"queued_contacts":2,"batchId":"0641fbdd-f62f-4c2c-ba02-3a83d5d11ac9",
+  # "message":"Contact import queued"}')
+  #     end
+  #   end
 end
